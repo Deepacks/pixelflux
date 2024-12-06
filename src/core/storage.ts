b961d70d265b4ad9ec46f363c1768237dae1b081
Deepacks <vladimirsette.vc@gmail.com>
2024-12-06 20:43:57 +0100

    feat: optimise uploads storage

--- src/core/storage.ts
+++ src/core/storage.ts
@@ -1,13 +1,36 @@
+import { Request } from 'express'
+import { mkdirSync } from 'fs'
 import { diskStorage } from 'multer'
-import { join, extname } from 'path'
+import { join } from 'path'
 import { v4 as uuid } from 'uuid'
 
+interface MulterRequest extends Request {
+  generatedFilename?: string
+}
+
 const storage = diskStorage({
-  destination: (_, __, cb) => {
-    cb(null, join(__dirname, '../..', '/uploads'))
+  destination: (req: MulterRequest, file, cb) => {
+    const generatedFilename = uuid()
+
+    req.generatedFilename = generatedFilename
+
+    const dirPath = [
+      generatedFilename.substring(0, 2),
+      generatedFilename.substring(2, 4),
+      generatedFilename.substring(4, 6),
+    ]
+
+    const destinationPath = join(__dirname, '../..', '/uploads', ...dirPath)
+    mkdirSync(destinationPath, { recursive: true })
+
+    cb(null, destinationPath)
   },
-  filename: (_, __, cb) => {
-    cb(null, uuid())
+  filename: (req: MulterRequest, __, cb) => {
+    if (!req.generatedFilename) {
+      return cb(new Error('Generated filename not found'), '')
+    }
+
+    cb(null, req.generatedFilename)
   },
 })
 
